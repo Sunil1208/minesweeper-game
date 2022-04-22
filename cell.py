8d37e379dc0cdf7c0a05c02de3e84a881a3c224e
sunil-kumar <sunil@loadshare.net>
2022-04-23 01:15:37 +0530

    added logic to show count of nearby mines and show mine

--- cell.py
+++ cell.py
@@ -19,7 +19,6 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f'{self.x},{self.y}'
         )
         btn.bind("<Button-1>", self.left_click_actions)
         btn.bind("<Button-2>", self.middle_click_actions)
@@ -27,8 +26,49 @@ class Cell:
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
-        print("event is ", event)
-        print("I am left clicked!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def show_mine(self):
+        # A logic to interrupt the game and display a message that player lost!
+        self.cell_btn_object.configure(bg="black", text='BOOM')
+
+    def show_cell(self):
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+    
+    #read only property
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+        return counter
+
+    #read only property
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y -1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+        cells = [cell for cell in cells if cell]
+        return cells
+
+
+    def get_cell_by_axis(self, x, y):
+        # Return a cell object based on the values of x,y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
     
     def right_click_actions(self, event):
         print("right click event is ", event)
